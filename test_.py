e289c57be0f6fff54e434e71e5e13438a6f1fa43
Dominik <dominik.czajka@onet.pl>
2026-07-14 11:20:03 +0200

    quickfix: spelling error in test file

--- test_.py
+++ test_.py
@@ -3,5 +3,5 @@ from main import app
 
 def test_hello():
     response = app.test_client().get('/')
-    assert response.staus_code == 200
+    assert response.status_code == 200
     assert response.data.decode('utf-8') == "Hello DevOps!"
